046d9fa89ac25ba6e14ccd74511298791a14b773
Abdul Saboor Sohaib <abdulsaboorsoahib@gmail.com>
2022-10-30 21:21:09 +0430

    Add implementation of to do list

---
+++ index.js
@@ -0,0 +1,40 @@
+const taskInput = document.getElementById('task-input');
+const addTask = document.getElementById('add-task');
+const taskContainer = document.getElementById('task-container');
+
+addTask.addEventListener('click', () => {
+
+    const task = document.createElement('div');
+    task.classList.add('task');
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('checked');
+    task.appendChild(checkbox);
+
+    const li = document.createElement('li');
+    li.innerText = `${taskInput.value}`;
+    task.appendChild(li);
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerHTML= '<i class="fa-solid fa-trash-can"></i>';
+    deleteBtn.classList.add('delete-btn');
+    task.appendChild(deleteBtn);
+        
+    if(taskInput.value === ""){
+        alert('plaes enter task');
+    } else {
+        taskContainer.appendChild(task);
+    }
+
+    taskInput.value = "";
+
+    checkbox.addEventListener('click', () => {
+        checkbox.parentElement.style.textDecoration = 'line-through';
+    })
+
+    deleteBtn.addEventListener('click', (e) => {
+        let target = e.target;
+        target.parentElement.parentElement.remove();
+    });
+});
